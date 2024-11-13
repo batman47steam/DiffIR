0328b27f37c0acb0b2f512a90dbf7d6d0e77381c
ComradeBionic <batman47steam@outlook.com>
2024-11-13 14:09:06 +0800

    update config and file path

--- DiffIR-demotionblur/generate_patches_gopro.py
+++ DiffIR-demotionblur/generate_patches_gopro.py
@@ -22,8 +22,8 @@ def train_files(file_):
     num_patch = 0
     w, h = lr_img.shape[:2]
     if w > p_max and h > p_max:
-        w1 = list(np.arange(0, w-patch_size, patch_size-overlap, dtype=np.int))
-        h1 = list(np.arange(0, h-patch_size, patch_size-overlap, dtype=np.int))
+        w1 = list(np.arange(0, w-patch_size, patch_size-overlap, dtype=np.int_))
+        h1 = list(np.arange(0, h-patch_size, patch_size-overlap, dtype=np.int_))
         w1.append(w-patch_size)
         h1.append(h-patch_size)
         for i in w1:
@@ -72,8 +72,10 @@ patch_size = 512
 overlap = 256
 p_max = 0
 
-src = '/mnt/bn/xiabinsr/datasets/restormer/deblur/train'
-tar = '/mnt/bn/xiabinsr/datasets/restormer/deblur/train_crops'
+#src = '/mnt/bn/xiabinsr/datasets/restormer/deblur/train'
+#tar = '/mnt/bn/xiabinsr/datasets/restormer/deblur/train_crops'
+src = 'Datasets/Downloads/GoPro'
+tar = 'Datasets/train/GoPro'
 
 lr_tar = os.path.join(tar, 'input_crops')
 hr_tar = os.path.join(tar, 'target_crops')
@@ -91,8 +93,11 @@ Parallel(n_jobs=num_cores)(delayed(train_files)(file_) for file_ in tqdm(files))
 
 ############ Prepare validation data ####################
 val_patch_size = 256
-src = '/mnt/bn/xiabinsr/datasets/restormer/deblur/test/GoPro'
-tar = '/mnt/bn/xiabinsr/datasets/restormer/deblur/val/GoPro'
+#src = '/mnt/bn/xiabinsr/datasets/restormer/deblur/test/GoPro'
+#tar = '/mnt/bn/xiabinsr/datasets/restormer/deblur/val/GoPro'
+
+src = 'Datasets/test/GoPro'
+tar = 'Datasets/val/GoPro'
 
 lr_tar = os.path.join(tar, 'input_crops')
 hr_tar = os.path.join(tar, 'target_crops')
